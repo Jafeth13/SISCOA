af7bfb4c6372c75e82580ef6dff3fbdf1874e15c
Jafeth13 <81998303+Jafeth13@users.noreply.github.com>
2022-10-05 11:15:33 -0600

    new
    new

--- SISCOA/src/app/office-list/office-list.component.ts
+++ SISCOA/src/app/office-list/office-list.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import {AfterViewInit, ViewChild} from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
 @Component({
   selector: 'app-office-list',
   templateUrl: './office-list.component.html',
   styleUrls: ['./office-list.component.css']
 })
-export class OfficeListComponent implements OnInit {
+//OfficeListComponent
+export class OfficeListComponent implements AfterViewInit {
+  displayedColumns: string[] = ['name', 'code','status','institution', 'action'];
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
 
-  constructor() { }
+  @ViewChild(MatPaginator) paginator :any = MatPaginator;
 
-  ngOnInit(): void {
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 
+  applyFilter(event:Event){
+    const filterValue=(event.target as HTMLInputElement).value;
+    this.dataSource.filter=filterValue.trim().toLowerCase();
+  }
 }
+
+
+export interface PeriodicElement {
+  name: string;
+  code: string;
+  status: string;
+  institution:string;
+}
+
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { name: 'Hydrogen', code: 'Matamoros',status:'Cordero',institution:'Cartago'},
+  { name: 'Hydrogen', code: 'Matamoros',status:'Cordero',institution:'Cartago'},
+  { name: 'Hydrogen', code: 'Matamoros',status:'Cordero',institution:'Cartago'},
+  { name: 'Hydrogen', code: 'Matamoros',status:'Cordero',institution:'Cartago'},
+  { name: 'Hydrogen', code: 'Matamoros',status:'Cordero',institution:'Cartago'},
+  { name: 'Hydrogen', code: 'Matamoros',status:'Cordero',institution:'Cartago'},
+  { name: 'Hydrogen', code: 'Matamoros',status:'Cordero',institution:'Cartago'},
+];
--- SISCOA/src/app/rol-list/rol-list.component.ts
+++ SISCOA/src/app/rol-list/rol-list.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import {AfterViewInit, ViewChild} from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'app-rol-list',
   templateUrl: './rol-list.component.html',
   styleUrls: ['./rol-list.component.css']
 })
-export class RolListComponent implements OnInit {
+export class RolListComponent implements AfterViewInit {
+  displayedColumns: string[] = ['name','action'];
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
 
-  constructor() { }
+  @ViewChild(MatPaginator) paginator :any = MatPaginator;
 
-  ngOnInit(): void {
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 
+  applyFilter(event:Event){
+    const filterValue=(event.target as HTMLInputElement).value;
+    this.dataSource.filter=filterValue.trim().toLowerCase();
+  }
 }
+
+
+export interface PeriodicElement {
+  name: string;
+ 
+}
+
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { name: 'Hydrogen'},
+  { name: 'Hydrogen'},  
+  { name: 'Hydrogen'},
+  { name: 'Hydrogen'},
+  { name: 'Hydrogen'},
+
+];
+
