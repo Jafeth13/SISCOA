622e15227f7b7a00c4d3907fa9c6e7e0c668b644
Jafeth13 <81998303+Jafeth13@users.noreply.github.com>
2022-10-07 13:22:19 -0600

    new
    new

---
+++ SISCOA/src/app/service-user.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, Observable, of, tap } from 'rxjs';
+const endpoint='https://localhost:44353/api/';
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    Authorization: 'Bearer'
+  })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServiceUserService {
+
+  constructor(private http: HttpClient) { }
+
+  
+  userList():Observable<any>{
+
+    return  this.http.get(endpoint+'Usuario',httpOptions);
+    
+  }
+  
+}
---
+++ SISCOA/src/app/services-office.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, Observable, of, tap } from 'rxjs';
+const endpoint='https://localhost:44353/api/';
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    Authorization: 'Bearer'
+  })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServicesOfficeService {
+
+  constructor(private http: HttpClient) { }
+
+  officeList():Observable<any>{
+
+    return  this.http.get(endpoint+'Oficinas',httpOptions);
+    
+  }
+
+}
