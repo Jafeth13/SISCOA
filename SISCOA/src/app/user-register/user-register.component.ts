f6cc6d4d785c46776e8ad6e65aa1d6329fb4104f
Jafeth13 <81998303+Jafeth13@users.noreply.github.com>
2022-10-24 23:36:26 -0600

    new
    new

--- SISCOA/src/app/user-register/user-register.component.ts
+++ SISCOA/src/app/user-register/user-register.component.ts
@@ -1,15 +1,28 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import Swal from 'sweetalert2';
+import { ServicesRolService } from '../services-rol.service';
+import { ServicesOfficeService } from '../services-office.service';
+
 @Component({
   selector: 'app-user-register',
   templateUrl: './user-register.component.html',
   styleUrls: ['./user-register.component.css']
 })
 export class UserRegisterComponent implements OnInit {
-
-  constructor(private route:ActivatedRoute,private router:Router) { }
-
+  @Input()userData:any;
+  constructor(public rest:ServicesRolService,public rest2:ServicesOfficeService,private route:ActivatedRoute,private router:Router) { }
+  roleData:any;
+  dataOffice:any;
   ngOnInit(): void {
+    this.rest.rolList().subscribe((pos)=>{
+      console.log(pos);
+      this.roleData=pos
+      });
+      this.rest2.officeList().subscribe((pos)=>{
+        console.log(pos);
+        this.dataOffice=pos
+        });
   }
 
 }
