bfa3dcb87f6f81d9d92276bfa68ea54c9e9553c2
Mario Quiros Luna <marioql.dev@gmail.com>
2022-10-30 09:08:09 -0600

    Add GetDataGraphics_ControlsSlopes, GetOfficeControlByIdOffice

--- SISCOA_BACK/Business/Services/IOficinaControlService.cs
+++ SISCOA_BACK/Business/Services/IOficinaControlService.cs
@@ -8,5 +8,7 @@ namespace Services.Services
     public interface IOficinaControlService : IGenericService<TSISCOA_OficinaControl>
     {
         Task<IEnumerable<TSISCOA_DataGraphics>> GetDataGraphics_ControlsByStates();
+        Task<IEnumerable<TSISCOA_DataGraphics>> GetDataGraphics_ControlsSlopes();
+        Task<IEnumerable<TSISCOA_OficinaControl>> GetOfficeControlByIdOffice(int id);
     }
 }
--- SISCOA_BACK/Data/Repositories/IOficinaControlRepository.cs
+++ SISCOA_BACK/Data/Repositories/IOficinaControlRepository.cs
@@ -8,5 +8,7 @@ namespace Repositories.Repositories
     public interface IOficinaControlRepository : IGenericRepository<TSISCOA_OficinaControl>
     {
         Task<IEnumerable<TSISCOA_DataGraphics>> GetDataGraphics_ControlsByStates();
+        Task<IEnumerable<TSISCOA_DataGraphics>> GetDataGraphics_ControlsSlopes();
+        Task<IEnumerable<TSISCOA_OficinaControl>> GetOfficeControlByIdOffice(int id);
     }
 }
