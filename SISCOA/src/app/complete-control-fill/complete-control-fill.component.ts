a0e872824b0d91fc284f4946e14a504ec0c98c4d
Jafeth13 <81998303+Jafeth13@users.noreply.github.com>
2022-11-10 00:44:26 -0600

    new
    new

--- SISCOA/src/app/complete-control-fill/complete-control-fill.component.ts
+++ SISCOA/src/app/complete-control-fill/complete-control-fill.component.ts
@@ -5,6 +5,8 @@ import { ServicesControllersService } from '../services-controllers.service';
 import { ServicesPeriodService } from '../services-period.service';
 import { ServiceConditionService } from '../service-condition.service';
 import { OfficeControlServicesService } from '../office-control-services.service';
+import { DomSanitizer } from '@angular/platform-browser';
+
 @Component({
   selector: 'app-complete-control-fill',
   templateUrl: './complete-control-fill.component.html',
@@ -22,7 +24,8 @@ export class CompleteControlFillComponent implements OnInit {
     public restOfficeControl: OfficeControlServicesService,
     public restConditional: ServiceConditionService,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private sanitizer: DomSanitizer
   ) {}
   ngOnInit(): void {
     this.rut();
@@ -35,7 +38,7 @@ export class CompleteControlFillComponent implements OnInit {
       .getControlId(this.route.snapshot.params['ID'], idU)
       .subscribe((pos) => {
         this.controlDataDelete = pos;
-        console.log(pos)
+        console.log(pos);
       });
 
     this.restPeriod.periodList(idU).subscribe((pos) => {
@@ -46,25 +49,56 @@ export class CompleteControlFillComponent implements OnInit {
     });
   }
 
-  add(){
-    let idU = localStorage.getItem("idUsuario");
-
-    this.restOfficeControl.update(this.controlDataDelete.ID,this.controlDataDelete, idU).subscribe(
-      (result) => {
-        Swal.fire('Buen trabajo!', 'Control completado!', 'success');
-       this.back()
+  add() {
+    let idU = localStorage.getItem('idUsuario');
+    const body = new FormData();
+   
+    
+   
+    console.log(this.controlDataDelete);
+    this.restOfficeControl
+      .update(this.controlDataDelete.ID, this.controlDataDelete, idU)
+      .subscribe(
+        (result) => {
+          Swal.fire('Buen trabajo!', 'Control completado!', 'success');
+          this.back();
+        },
+        (err) => {
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Something went wrong!',
+          });
+        }
+      );
   }
-      ,
-      (err) => {
-        Swal.fire({
-          icon: 'error',
-          title: 'Oops...',
-          text: 'Something went wrong!',
-        });
-      }
-    );
-  }
-  back(){
+  back() {
     this.router.navigate(['/controlOfice']);
   }
+  private fileTemp: any;
+  getFile($event: any) {
+    const [file] = $event.target.files;
+    this.controlDataDelete.Archivos.TC_Nombre = this.fileTemp.fileName;
+    this.controlDataDelete.Archivos.FK_TN_OficinaControl_SISCOA_Archivo =this.controlDataDelete.ID;
+    this.controlDataDelete.Archivos.TC_Datos=this.getBase64(file);
+    console.log(this.controlDataDelete)
+    this.fileTemp = {
+      fileRaw: file,
+      fileName: file.name,
+    };
+  }
+  doc:any
+   getBase64(file:any) {
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function () {
+     
+      return reader.result;
+    };
+    reader.onerror = function (error) {
+      console.log('Error: ', error);
+    };
+ }
+
+
 }
